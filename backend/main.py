46535b5fd3a213fcce0780cd35ed59d3d35cf23b
olegp22 <oprotopopov924@gmail.com>
2026-04-29 15:45:07 +0500

    добавил адреса по которым можно делать запрос

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,24 @@ from fastapi import FastAPI
 from app.api.routers.auth import auth_router
 from app.api.routers.groups import groups_router
 from app.api.routers.users import users_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Список адресов, с которых можно делать запросы (пока локально)
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+     "http://127.0.0.1:5500", # если использует Vite
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # Разрешаем все методы (GET, POST, PUT, DELETE)
+    allow_headers=["*"], # Разрешаем все заголовки (включая Authorization)
+)
 
 app.include_router(users_router)
 
