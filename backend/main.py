91c12e0c0cdf03dd573cd5fb904df78e4a1153b5
olegp22 <oprotopopov924@gmail.com>
2026-06-17 04:09:59 +0500

    добаавил адресс

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ origins = [
     "http://localhost:3000",
     "http://localhost:5173",
      "http://127.0.0.1:5500", # если использует Vite
-     "https://your-frontend.github.io"
+     "https://project-workshop-8s9jvp7am-olegp222.vercel.app"
 ]
 
 app.add_middleware(
